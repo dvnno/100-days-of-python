39347e894eb3db97c67c8fe6018146755d56abc5
Devin North <13654540+dvnno@users.noreply.github.com>
2024-09-02 22:58:16 -0500

    It can be very humbling to intentionally go through the most basic levels of learning a language when you a) have some experience with the language, and b) you are reasonably experienced as a programmer. That being said, I committed to completing the 100 days as they are and I think there is some value in that commitment, even if I'm retreading old ground. It's humbling.

--- day1/task.py
+++ day1/task.py
@@ -1,1 +1,20 @@
-print("Hello world!")
+print("Hello world!\nHello world!")
+print("Hello" + " " + "world!")
+print("Hello " + input("What is your name? "))
+print("Hello " + input("What is your name? ") + "!")
+# Comment
+name = input("I'm asking you one more time, what is your name? ")
+print("Hello " + name + "!")
+
+name = "Jack"
+print("Hello " + name + "!")
+name = "Jill"
+print("Hello " + name + "!")
+
+print(len(name))
+
+print(len(input("What is your name? ")))
+
+username = input("What is your username? ")
+length = len(username)
+print(length)
