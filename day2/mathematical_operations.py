aeae0157063c46bdd52054ad4567d93762e9c0bd
Devin North <13654540+dvnno@users.noreply.github.com>
2024-09-03 22:46:56 -0500

    completed mathematical operations section

---
+++ day2/mathematical_operations.py
@@ -0,0 +1,14 @@
+import math
+
+print(123 + 456)
+print(123 - 456)
+print(123 * 456)
+print(123 / 456)
+print(123 // 456) # converts to an integer (floor round)
+print(123 % 456)
+print(123 ** 456) # exponentiation
+print(math.floor(math.sin(3.141592653589793)))
+
+# follows order of operations
+print(3 * 3 + 3 / 3 - 3) # 7.0
+print(3 * (3 + 3) / 3 - 3) # 3.0
